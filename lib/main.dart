4c08a8b78eb6fe18c07b753cc2aada1f3976865a
deluscc4 <delusccafs@icloud.com>
2024-02-15 23:05:29 -0300

    my studies

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(MarimbaApp());
 
 class MarimbaApp extends StatelessWidget {
+  void tocarSom(int numeroSom) {
+    final player = AudioPlayer();
+    player.play(AssetSource('nota$numeroSom.wav'));
+  }
+
+  Expanded criarBotao({required Color cor, required int numeroSom}) {
+    return Expanded(
+      child: Container(
+        color: cor,
+        child:
+        TextButton(
+          onPressed: () {
+            tocarSom(numeroSom);
+          },
+          child: Text(''),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              criarBotao(cor: Colors.green.shade900, numeroSom: 1),
+              criarBotao(cor: Colors.blue.shade900, numeroSom: 2),
+              criarBotao(cor: Colors.yellowAccent, numeroSom: 3),
+              criarBotao(cor: Colors.deepOrange, numeroSom: 4),
+              criarBotao(cor: Colors.blueGrey, numeroSom: 5),
+              criarBotao(cor: Colors.purpleAccent.shade200, numeroSom: 6),
+              criarBotao(cor: Colors.brown.shade800, numeroSom: 7),
+            ],
+          ),
         ),
       ),
     );
